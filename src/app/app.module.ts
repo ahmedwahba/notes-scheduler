3c1a8f19ef2ca7d2835d9839db15c72d7de684c6
AhmedWahba <ahmed.wahba1990@gmail.com>
2022-03-04 23:00:37 +0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,58 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatCardModule,
+  MatIconModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDialogModule,
+  MatTooltipModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NotesTableComponent } from './components/notes-table/notes-table.component';
+import { NotesStackGridComponent } from './components/notes-stack-grid/notes-stack-grid.component';
+import { NoteDetailsComponent } from './components/note-details/note-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesTableComponent,
+    NotesStackGridComponent,
+    NoteDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  entryComponents: [NoteDetailsComponent],
+  providers: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
